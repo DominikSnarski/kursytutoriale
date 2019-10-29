15c77ce27047b2a81c5da5342a1a6de7319d01c3
Jakub Lotko <1elstac1@gmail.com>
2019-10-29 08:21:54 +0100

    Add project files.

---
+++ KursyTutoriale.API/Controllers/ExampleController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace KursyTutoriale.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ExampleController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "A", "B" };
+        }
+    }
+}
