26b3c4516eaf52a28d79d56ad390678662e917ce
B.Olchowik <wokoren1@gmail.com>
2019-10-31 22:51:12 +0100

    HomePage, szkielet, lista kursow

---
+++ KursyTutoriale.API/ClientApp/src/components/List/ShowPagination.js
@@ -0,0 +1,66 @@
+import React, { Fragment } from 'react';
+import Pagination from './Pagination';
+import { Table, Media, Jumbotron, Container } from 'reactstrap';
+
+class ShowPagination extends React.Component {
+    constructor() {
+        super();
+
+        // an example array of items to be paged
+        var exampleItems = [...Array(100).keys()].map(i => ({ id: (i+1), name: 'Item ' + (i+1), category:(i+2) }));
+
+        this.state = {
+            exampleItems: exampleItems,
+            pageOfItems: []
+        };
+
+        // bind function in constructor instead of render (https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-no-bind.md)
+        this.onChangePage = this.onChangePage.bind(this);
+    }
+
+    onChangePage(pageOfItems) {
+        // update state with new page of items
+        this.setState({ pageOfItems: pageOfItems });
+    }
+
+    render() {
+        return (
+            <Container>
+                <Jumbotron fluid className="jumbotron_bg">
+                    <span className="d-lg-flex justify-content-center d-block h2 text-dark">Courses</span>
+                </Jumbotron>
+                <div>
+                        <Table>
+                        <thead>
+                        <tr>
+                        <th scope="row">#</th>
+                        <th>Image</th>
+                        <th>Title</th>
+                        <th>Category</th>
+                        </tr>
+                        </thead>
+                        {this.state.pageOfItems.map(item =>
+                            <tbody>
+                            <tr onClick={() => { alert("test") }}
+                            style={{ cursor: 'pointer' }}>
+                            <td>{item.id}</td>
+                            <td>      
+                            <Media src="https://jakewilson.gallerycdn.vsassets.io/extensions/jakewilson/vscode-placeholder-images/0.1.0/1499508629226/Microsoft.VisualStudio.Services.Icons.Default" />
+                            </td>
+                            <td>{item.name}</td>
+                            <td>{item.category}</td>
+                            </tr>
+                            </tbody>
+                        )}
+                        </Table>
+                        <Pagination items={this.state.exampleItems} onChangePage={this.onChangePage} />
+                        
+
+                </div>
+                <hr />
+            </Container>
+        );
+    }
+}
+
+export default ShowPagination;
