2c1c78c8e596ad512094491675abdcde8be012f1
Jakub Lotko <1elstac1@gmail.com>
2019-11-16 12:07:40 +0100

    Create dbcontext and design time factory

---
+++ KursyTutoriale.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using KursyTutoriale.Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace KursyTutoriale.Infrastructure
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser,IdentityRole<Guid>,Guid>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+
+    }
+}
