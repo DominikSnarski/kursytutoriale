809accd2e7a03b877c413ff2ac189205ad32a6da
Jakub Lotko <1elstac1@gmail.com>
2019-11-17 10:22:51 +0100

    Add JWTTokenDto

---
+++ KursyTutoriale.Application/DataTransferObjects/Auth/JWTTokenDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KursyTutoriale.Application.DataTransferObjects.Auth
+{
+    public class JWTTokenDto
+    {
+        public string Token { get; set; }
+    }
+}
