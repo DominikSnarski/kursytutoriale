585a27152aa59c0809b03477b8fa0e3f215bb79e
Mateusz Piotrowski <matipio98@wp.pl>
2019-11-24 00:25:31 +0100

    Added User Basic Information DTO

---
+++ KursyTutoriale.Application/DataTransferObjects/UserDto/UserBasicInformationDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KursyTutoriale.Application.DataTransferObjects
+{
+    class UserBasicInformationDTO
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Description { get; set; }
+    }
+}
