6fde04cae1c07d506177cf7375b2e13d31a4aff0
Jakub Lotko <1elstac1@gmail.com>
2019-11-28 21:50:33 +0100

    Add sql script to populate db with default data

---
+++ KursyTutoriale.Infrastructure/SqlScripts/DefaultValues.sql
@@ -0,0 +1,11 @@
+﻿PRINT N'Tags_JL_0_0 begins';
+BEGIN TRAN Tags_JL_0_0;
+	IF NOT EXISTS (SELECT * FROM KTTag WHERE Name='Cooking') INSERT INTO KTTag(Id,Name) VALUES (NEWID(),'Cooking');
+	IF NOT EXISTS (SELECT * FROM KTTag WHERE Name='Business') INSERT INTO KTTag(Id,Name) VALUES (NEWID(),'Business');
+	IF NOT EXISTS (SELECT * FROM KTTag WHERE Name='Design') INSERT INTO KTTag(Id,Name) VALUES (NEWID(),'Design');
+	IF NOT EXISTS (SELECT * FROM KTTag WHERE Name='Illustration') INSERT INTO KTTag(Id,Name) VALUES (NEWID(),'Illustration');
+	IF NOT EXISTS (SELECT * FROM KTTag WHERE Name='Entrepreneurship') INSERT INTO KTTag(Id,Name) VALUES (NEWID(),'Entrepreneurship');
+	IF NOT EXISTS (SELECT * FROM KTTag WHERE Name='Writing') INSERT INTO KTTag(Id,Name) VALUES (NEWID(),'Writing');
+	IF NOT EXISTS (SELECT * FROM KTTag WHERE Name='Technology') INSERT INTO KTTag(Id,Name) VALUES (NEWID(),'Technology');
+COMMIT TRAN Tags_JL_0_0;
+PRINT N'Tags_JL_0_0 completed';
