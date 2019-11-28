93f4f42ba1912e19b268e0f4a1cf20a5c2529b21
Jakub Lotko <1elstac1@gmail.com>
2019-11-28 21:51:41 +0100

    Add TagDto and mapping

---
+++ KursyTutoriale.Application/DataTransferObjects/Tags/TagDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace KursyTutoriale.Application.DataTransferObjects.Tags
+{
+    public class TagDTO
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
