8889a6f17bf555fa09ed384207c9a1ac6cea5a70
Jakub Lotko <1elstac1@gmail.com>
2019-11-28 21:52:03 +0100

    Add tag service with getAllTags method

---
+++ KursyTutoriale.Application/Services/Tags/TagService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using KursyTutoriale.Application.DataTransferObjects.Tags;
+using KursyTutoriale.Domain.Entities.Course;
+using KursyTutoriale.Infrastructure.Repositories;
+
+namespace KursyTutoriale.Application.Services.Tags
+{
+    class TagService : ITagService
+    {
+        IExtendedRepository<Tag> repository;
+        IDTOMapper mapper;
+
+        public TagService(IExtendedRepository<Tag> repository, IDTOMapper mapper)
+        {
+            this.repository = repository;
+            this.mapper = mapper;
+        }
+
+        public ICollection<TagDTO> GetAllTags()
+        {
+            var tagsCollection = repository.Queryable().ToList();
+
+            return mapper.Map<List<TagDTO>>(tagsCollection);
+        }
+    }
+}
