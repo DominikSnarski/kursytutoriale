70636a4f435db1665752d2f2354435a76434b91b
Mateusz Piotrowski <matipio98@wp.pl>
2019-11-30 20:20:52 +0000

    Update LessonForEditionDTO.cs

--- KursyTutoriale.Application/DataTransferObjects/Course/LessonForEditionDTO.cs
+++ KursyTutoriale.Application/DataTransferObjects/Course/LessonForEditionDTO.cs
@@ -9,13 +9,4 @@ namespace KursyTutoriale.Application.DataTransferObjects.Course
         public int Index { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
-<<<<<<< HEAD:KursyTutoriale.Application/DataTransferObjects/Course/LessonForEditionDTO.cs
-=======
-        public Guid AuthorId { get; set; }
-        public DateTime Date { get; set; }
-        public DateTime DateOfLastEdit { get; set; }
-        public int Rating { get; set; }
-        public int Popularity { get; set; }
->>>>>>> Added Featured courses to course service:KursyTutoriale.Domain/Entities/Course.cs
-    }
 }
