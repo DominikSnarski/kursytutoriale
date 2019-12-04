6ef326a99c6b1d9486ebfe29662d07b72a3931df
Jakub Lotko <1elstac1@gmail.com>
2019-12-04 21:02:29 +0100

    Add generic response classes

---
+++ KursyTutoriale.API/Responses/DtoBasedRsponse.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace KursyTutoriale.API.Responses
+{
+    public class DtoBasedRsponse<TDto>:GenericResponse
+    {
+        public DtoBasedRsponse():base()
+        {
+
+        }
+
+        public DtoBasedRsponse(TDto data) : base(200)
+        {
+            Payload = data;
+        }
+
+        public DtoBasedRsponse(int code, string description) : base(code,description)
+        {
+
+        }
+
+        public DtoBasedRsponse(int code, IEnumerable<string> messages) : base(code, messages)
+        {
+
+        }
+
+        public TDto Payload { get; set; }
+
+    }
+}
---
+++ KursyTutoriale.API/Responses/GenericResponse.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace KursyTutoriale.API.Responses
+{
+    public class GenericResponse
+    {
+        public GenericResponse(){ }
+        public GenericResponse(int code)
+        {
+            Code = code;
+            Description = "";
+        }
+
+        public GenericResponse(int code, string description)
+        {
+            Code = code;
+            Description = description;
+        }
+
+        public GenericResponse(int code, IEnumerable<string> messages)
+        {
+            Code = code;
+
+            var sb = new StringBuilder();
+
+            foreach (var message in messages)
+                sb.Append(message + "\n");
+
+            Description = sb.ToString();
+        }
+
+        public int Code { get; set; }
+        public string Description { get; set; }
+    }
+}
