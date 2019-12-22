129284a7a6703b265835dc164c169cee736dce98
Mateusz Piotrowski <matipio98@wp.pl>
2019-12-22 20:49:34 +0100

    Changed the way Course Details are displayed on the list

--- KursyTutoriale.WebApp/ClientApp/src/api/Services/CourseService.js
+++ KursyTutoriale.WebApp/ClientApp/src/api/Services/CourseService.js
@@ -4,18 +4,10 @@ export const CourseService = {
     getCoursePages: (firstPage,lastPage) => {
         return apiClient.get('api/CoursesViewer/GetNumberOfCourses')
             .then(response => {
-                var nrOfCourses=response.data;
                 var nrOfPages=Math.ceil(response.data/4)-1;
                 apiClient.get('api/CoursesViewer/GetPagesOfCourses?firstPageNumber='+firstPage+'&lastPageNumber='+nrOfPages+'&pageSize=4')
-                .then(response => {  
-                    var example=[...Array(nrOfCourses).keys()]
-                    .map(i => ({
-                         id: (i + 1),
-                         name: response.data[i].title,
-                         date: response.data[i].date 
-                    }));
-
-                    return new Promise((resolve)=>resolve(example));
+                .then(res => {  
+                    return new Promise((resolve)=>resolve(res.data));
                 });
 
             }, error => {return new Promise((resolve,reject)=>reject(error))});
--- KursyTutoriale.WebApp/ClientApp/src/components/Details/Details.jsx
+++ KursyTutoriale.WebApp/ClientApp/src/components/Details/Details.jsx
@@ -1,73 +1,50 @@
 import React from 'react';
 import "./Details.css"
-import { Container, Row, Col, Alert, Spinner } from 'reactstrap';
-import fetchDetails from '../../api/Services/DetailsService';
+import { Container, Row, Col, Collapse,Media } from 'reactstrap';
+import {AppRoutes} from '../../routing/AppRoutes';
+import {Link} from 'react-router-dom';
 
 class Details extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      isLoading: false,
-      error: false,
-      title: '',
-      price: '',
-      description: '',
-      category: '',
-      tags: ['yes']
-    };
+      course: this.props.course,
+      isOpen: false
+    }
   }
 
-  componentDidMount(){
-    this.setState({ isLoading: true });
-
-    fetchDetails(this.props.id, this);        
-}
+  toggle(){ this.setState({isOpen:!this.state.isOpen}); }
 
   render() {
-
-    if (this.state.error) 
-      return (
-        <Row><Col xs="6" sm="4"></Col>
-          <Col sm="12" md={{ size: 10, offset: 1 }}><Alert color="danger">Something went terribly wrong.</Alert></Col>
-          <Col sm="4"></Col></Row>
-      )
-
-    if (this.state.isLoading)
-      return (
-        <Row><Col xs="6" sm="4"></Col>
-          <Col xs="6" sm="4"><Spinner className="d-lg-flex d-block h2" style={{ width: '3rem', height: '3rem' }} color="primary" /></Col>
-          <Col sm="4"></Col></Row>
-      )
-
     return (
-      <Container>
+      <tbody>
+          <tr onClick={() => this.toggle()}
+              style={{ cursor: 'pointer' }}>
+              <td>
+                  <Media src="https://jakewilson.gallerycdn.vsassets.io/extensions/jakewilson/vscode-placeholder-images/0.1.0/1499508629226/Microsoft.VisualStudio.Services.Icons.Default" />
+              </td>
+              <Link to={AppRoutes.Courseview+"/"+this.state.course.id}><td>{this.state.course.title}</td></Link>
+              <td>{this.state.course.date}</td>
+          </tr>
+          <Collapse isOpen={this.state.isOpen}>
+          <Container>
+            <Row>
+              <Col className="additional">
+                Price: {this.state.course.price}
+              </Col>
+            </Row>
 
-        <Row>
-          <Col className="d-flex justify-content-center mb-2">
-            <h2>{this.state.title}</h2>
-          </Col>
-        </Row>
+            <Row>
+              <Col className="d-flex justify-content-center mb-2">
+                {this.state.course.description}
+              </Col>
+            </Row>
+          </Container>
+          </Collapse>
+      </tbody>
 
-        <Row>
-          <Col className="d-flex justify-content-center mb-2">
-            <img src="https://via.placeholder.com/320x200" alt="Generic placeholder"/>
-          </Col>
-        </Row>
 
-        <Row>
-          <Col className="additional">
-            Tags:{this.state.tags.map(txt => <span> {txt.id}</span>)};  Price: {this.state.price}
-          </Col>
-        </Row>
-
-        <Row>
-          <Col className="d-flex justify-content-center mb-2">
-            {this.state.description}
-      </Col>
-        </Row>
-
-      </Container>
     );
   }
 }
