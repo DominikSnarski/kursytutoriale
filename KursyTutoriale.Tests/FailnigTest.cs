8f7a797b3470693f8cf71a1ad44d90ea29d09542
Jakub Lotko <1elstac1@gmail.com>
2020-02-26 22:36:48 +0100

    Add failing test

---
+++ KursyTutoriale.Tests/FailnigTest.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace KursyTutoriale.Tests
+{
+    public class FailnigTest
+    {
+        [Fact]
+        public void Fail()
+        {
+            Assert.False(true);
+        }
+    }
+}
