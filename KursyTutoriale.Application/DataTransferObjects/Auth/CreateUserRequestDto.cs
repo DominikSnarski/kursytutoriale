dc273b7f35d0c761703ca6314d98f2e7712bab44
Maciej Klimowicz <maciek.klimowicz@gmail.com>
2020-03-03 19:55:03 +0100

    Summary
    Validation added

--- KursyTutoriale.Application/DataTransferObjects/Auth/CreateUserRequestDto.cs
+++ KursyTutoriale.Application/DataTransferObjects/Auth/CreateUserRequestDto.cs
@@ -1,9 +1,21 @@
-﻿namespace KursyTutoriale.Application.DataTransferObjects.Auth
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KursyTutoriale.Application.DataTransferObjects.Auth
 {
     public class CreateUserRequestDto
     {
+        [Display(Name = "Login")]
+        [Required(ErrorMessage = "Username is required!")]
+        [MinLength(5, ErrorMessage = "Username is too short! (5 characters minimum)")]
         public string Username { get; set; }
+
+        [Display(Name = "Password")]
+        [MinLength(3, ErrorMessage = "Password is too short! (5 characters minimum)")]
+        [Required(ErrorMessage = "Password is required!")]
         public string Password { get; set; }
+
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "Email is required!")]
         public string Email { get; set; }
     }
 }
