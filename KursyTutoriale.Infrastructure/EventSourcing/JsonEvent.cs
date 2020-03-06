13c853fc067263e3d41c4dd7efec17f43043f405
Jakub Lotko <1elstac1@gmail.com>
2020-03-06 22:04:38 +0100

    Add json event

---
+++ KursyTutoriale.Infrastructure/EventSourcing/JsonEvent.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KursyTutoriale.Infrastructure.EventSourcing
+{
+    public class JsonEvent
+    {
+        public JsonEvent()
+        {
+
+        }
+
+        public JsonEvent(
+            Guid id,
+            DateTime occuranceDate,
+            string data,
+            string eventType,
+            Guid entityId)
+        {
+            Id = id;
+            OccuranceDate = occuranceDate;
+            Data = data;
+            Type = eventType;
+            EntityId = entityId;
+        }
+
+        public Guid Id { get; private set; }
+        public DateTime OccuranceDate { get; private set; }
+        public string Data { get; private set; }
+        public string Type { get; private set; }
+        public Guid EntityId { get; private set; }
+    }
+}
