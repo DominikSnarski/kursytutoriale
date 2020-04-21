9f75013d6003d1f184726bf68d800e350b33527b
Jakub Lotko <1elstac1@gmail.com>
2020-04-21 20:22:42 +0200

    Fix service

--- KursyTutoriale.WebApp/ClientApp/src/api/Services/PaymentService.js
+++ KursyTutoriale.WebApp/ClientApp/src/api/Services/PaymentService.js
@@ -1,21 +1,31 @@
 import apiClient from '../ApiClient';
 
 export const PaymentService = {
-    newPayment: (courseId, name, surname, cardNumber, expirationDateMonth, expirationDateYear, cvv) => {
-        return new Promise((resolve, reject) =>
-          apiClient
-            .post('/api/CreditCardPayment/PayForCourseAccess', {
-                courseId, 
-                name, 
-                surname, 
-                cardNumber, 
-                expirationDateMonth, 
-                expirationDateYear, 
-                cvv
-            })
-            .then((resp) => resolve(resp))
-            .catch((error) => reject(error)),
-        );
-      }
+  newPayment: (
+    courseId,
+    ownerFirstName,
+    ownerLastName,
+    number,
+    expMonth,
+    expYear,
+    cvv,
+  ) => {
+    return new Promise((resolve, reject) =>
+      apiClient
+        .post(
+          `/api/CreditCardPayment/PayForCourseAccess?courseId=${courseId}`,
+          {
+            ownerFirstName,
+            ownerLastName,
+            number,
+            expMonth,
+            expYear,
+            cvv,
+          },
+        )
+        .then((resp) => resolve(resp))
+        .catch((error) => reject(error)),
+    );
+  },
 };
 export default PaymentService;
