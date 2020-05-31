4ba63c5ece02ce35f3386cf85ea26984c35d5637
Bartosz Ziolkowski <ygbhnjm@gmail.com>
2020-05-31 15:45:51 +0200

    added validation to the rest of components

--- KursyTutoriale.WebApp/ClientApp/src/components/Editors/EditModule.jsx
+++ KursyTutoriale.WebApp/ClientApp/src/components/Editors/EditModule.jsx
@@ -1,52 +1,31 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Formik, Form } from 'formik';
+import * as Yup from 'yup';
 import { useHistory } from 'react-router-dom';
 import { ModuleService } from '../../api/Services/ModuleService';
+import ErrorMessage from '../../layouts/CSS/ErrorMessage/ErrorMessage';
 
 // eslint-disable-next-line
-import {
-  Form,
-  FormGroup,
-  FormFeedback,
-  Row,
-  Col,
-  Container,
-  Input,
-} from 'reactstrap';
+import { FormGroup, Row, Col, Container, Input } from 'reactstrap';
 
 import Button from '../../layouts/CSS/Button/Button';
 
 function NewModule(props) {
   const history = useHistory();
 
-  const [titleErrorMessage, setTitleErrorMessage] = useState('');
-  const [descriptionErrorMessage, setDescriptionErrorMessage] = useState('');
-  const [title, setTitle] = useState(props.location.state.title);
-  const [description, setDescription] = useState(
-    props.location.state.description,
-  );
+  const newModuleSchema = Yup.object().shape({
+    title: Yup.string()
+      .max(30, 'Title can contains max 30 characters')
+      .required('Field required'),
+    description: Yup.string().required('Field required'),
+  });
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    const formData = new FormData(event.target);
-
-    if (formData.get('title') === '') {
-      setTitleErrorMessage("Title can't be empty");
-    }
-
-    if (formData.get('description') === '') {
-      setDescriptionErrorMessage("Description can't be empty");
-    }
-
-    if (formData.get('description') === '' || formData.get('title') === '') {
-      return;
-    }
-
+  const handleSubmit = ({ title, description }) => {
     ModuleService.editModule(
       props.location.state.courseid,
       props.location.state.moduleid,
-      formData.get('title'),
-      formData.get('description'),
+      title,
+      description,
     ).then(() => {
       history.push(`/courseview/${props.location.state.courseid}`);
     });
@@ -54,62 +33,75 @@ function NewModule(props) {
 
   return (
     <Container className="Container">
-      <Form onSubmit={(e) => handleSubmit(e)}>
-        <Row></Row>
-        <Row className="mt-2">
-          <Col>
-            1. What will be the title of the module?
-            <FormGroup className="mt-2">
-              <Input
-                className="input_field mb-3"
-                type="text"
-                name="title"
-                id="titleField"
-                placeholder="Lesson title, Max. 100 characters"
-                onChange={(e) => {
-                  setTitle(e.target.value);
-                }}
-                value={title}
-              />
-              <FormFeedback valid>Sweet! that name is available</FormFeedback>
-            </FormGroup>
-            <span className="errorMessage">{titleErrorMessage}</span>
-          </Col>
-        </Row>
-        <Row className="justify-content-center text-dark mt-2">
-          <Col className="text-dark">
-            2. What will you learn in this module?
-            <FormGroup className="mt-2">
-              <Input
-                className="input_field mb-3"
-                type="text"
-                name="description"
-                id="titleField"
-                placeholder="Lesson description"
-                value={description}
-                onChange={(e) => {
-                  setDescription(e.target.value);
-                }}
-              />
-            </FormGroup>
-            <span className="errorMessage">{descriptionErrorMessage}</span>
-          </Col>
-        </Row>
+      <Row></Row>
+      <Formik
+        initialValues={{
+          title: props.location.state.title,
+          description: props.location.state.description,
+        }}
+        validationSchema={newModuleSchema}
+        onSubmit={(values) => handleSubmit(values)}
+      >
+        {({ errors, handleChange, values }) => (
+          <Form>
+            <Row className="mt-2">
+              <Col>
+                1. What will be the title of the module?
+                <FormGroup className="mt-2">
+                  <ErrorMessage message={errors.title} error={!!errors.title}>
+                    <Input
+                      type="text"
+                      name="title"
+                      id="title"
+                      placeholder="e.g. Breakfasts"
+                      className="input_field"
+                      value={values.title}
+                      onChange={handleChange}
+                    />
+                  </ErrorMessage>
+                </FormGroup>
+              </Col>
+            </Row>
+            <Row className="justify-content-center text-dark mt-2">
+              <Col className="text-dark">
+                2. What will you learn in this module?
+                <FormGroup className="mt-2">
+                  <ErrorMessage
+                    message={errors.description}
+                    error={!!errors.description}
+                  >
+                    <Input
+                      type="textarea"
+                      name="description"
+                      id="exampleText"
+                      placeholder="e.g. In this module you will learn how..."
+                      className="input_field"
+                      value={values.description}
+                      onChange={handleChange}
+                    />
+                  </ErrorMessage>
+                </FormGroup>
+              </Col>
+            </Row>
 
-        <Row className="mt-5">
-          <Col>
-            <Button
-              text="Back"
-              onClick={() => {
-                history.push(`/courseview/${props.location.state.courseid}`);
-              }}
-            ></Button>
-          </Col>
-          <Col className="text-right">
-            <Button text="Submit"></Button>
-          </Col>
-        </Row>
-      </Form>
+            <Row className="mt-5">
+              <Col>
+                <Button
+                  text="Back"
+                  onClick={() => {
+                    history.push(
+                      `/courseview/${props.location.state.courseid}`,
+                    );
+                  }}
+                ></Button>
+              </Col>
+              <Col className="text-right">
+                <Button text="Submit"></Button>
+              </Col>
+            </Row>
+          </Form>
+        )}
+      </Formik>
       <br />
     </Container>
   );
--- KursyTutoriale.WebApp/ClientApp/src/components/NewCourse/NewModule.jsx
+++ KursyTutoriale.WebApp/ClientApp/src/components/NewCourse/NewModule.jsx
@@ -1,51 +1,30 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Formik, Form } from 'formik';
+import * as Yup from 'yup';
 import { useHistory } from 'react-router-dom';
 import { ModuleService } from '../../api/Services/ModuleService';
+import ErrorMessage from '../../layouts/CSS/ErrorMessage/ErrorMessage';
 
 // eslint-disable-next-line
-import {
-  Form,
-  FormGroup,
-  FormFeedback,
-  Row,
-  Col,
-  Container,
-  Input,
-} from 'reactstrap';
+import { FormGroup, Row, Col, Container, Input } from 'reactstrap';
 
 import Button from '../../layouts/CSS/Button/Button';
 
 function NewModule(props) {
   const history = useHistory();
-  const [titleErrorMessage, setTitleErrorMessage] = useState('');
-  const [descriptionErrorMessage, setDescriptionErrorMessage] = useState('');
+ 
+  const newModuleSchema = Yup.object().shape({
+    title: Yup.string()
+      .max(30, 'Title can contains max 30 characters')
+      .required('Field required'),
+    description: Yup.string().required('Field required'),
+  });
 
-  const handleTextChange = () => {
-    setTitleErrorMessage('');
-    setDescriptionErrorMessage('');
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    const formData = new FormData(event.target);
-
-    if (formData.get('title') === '') {
-      setTitleErrorMessage("Title can't be empty");
-    }
-
-    if (formData.get('description') === '') {
-      setDescriptionErrorMessage("Description can't be empty");
-    }
-
-    if (formData.get('description') === '' || formData.get('title') === '') {
-      return;
-    }
-
+  const handleSubmit = ({ title, description }) => {
     ModuleService.addModule(
       props.location.state.courseid,
-      formData.get('title'),
-      formData.get('description'),
+      title,
+      description,
       'there will be no image here. Please delete me!',
     ).then(() => {
       history.push(`/courseview/${props.location.state.courseid}`);
@@ -54,56 +33,69 @@ function NewModule(props) {
 
   return (
     <Container className="Container">
-      <Form onSubmit={(e) => handleSubmit(e)}>
-        <Row></Row>
-        <Row className="mt-2">
-          <Col>
-            1. What will be the title of the module?
-            <FormGroup className="mt-2">
-              <Input
-                type="text"
-                name="title"
-                id="title"
-                placeholder="e.g. Breakfasts"
-                className="input_field"
-                onChange={handleTextChange}
-              />
-              <FormFeedback valid>Sweet! that name is available</FormFeedback>
-            </FormGroup>
-            <span className="errorMessage">{titleErrorMessage}</span>
-          </Col>
-        </Row>
-        <Row className="justify-content-center text-dark mt-2">
-          <Col className="text-dark">
-            2. What will you learn in this module?
-            <FormGroup className="mt-2">
-              <Input
-                type="textarea"
-                name="description"
-                id="exampleText"
-                placeholder="e.g. In this module you will learn how..."
-                className="input_field"
-                onChange={handleTextChange}
-              />
-            </FormGroup>
-            <span className="errorMessage">{descriptionErrorMessage}</span>
-          </Col>
-        </Row>
+      <Row></Row>
+      <Formik
+        initialValues={{ title: '', description: '' }}
+        validationSchema={newModuleSchema}
+        onSubmit={(values) => handleSubmit(values)}
+      >
+        {({ errors, handleChange, values }) => (
+          <Form>
+            <Row className="mt-2">
+              <Col>
+                1. What will be the title of the module?
+                <FormGroup className="mt-2">
+                  <ErrorMessage message={errors.title} error={!!errors.title}>
+                    <Input
+                      type="text"
+                      name="title"
+                      id="title"
+                      placeholder="e.g. Breakfasts"
+                      className="input_field"
+                      value={values.title}
+                      onChange={handleChange}
+                    />
+                  </ErrorMessage>
+                </FormGroup>
+              </Col>
+            </Row>
+            <Row className="justify-content-center text-dark mt-2">
+              <Col className="text-dark">
+                2. What will you learn in this module?
+                <FormGroup className="mt-2">
+                  <ErrorMessage message={errors.description} error={!!errors.description}>
+                    <Input
+                      type="textarea"
+                      name="description"
+                      id="exampleText"
+                      placeholder="e.g. In this module you will learn how..."
+                      className="input_field"
+                      value={values.description}
+                      onChange={handleChange}
+                    />
+                  </ErrorMessage>
+                </FormGroup>
+              </Col>
+            </Row>
 
-        <Row className="mt-5">
-          <Col>
-            <Button
-              text="Back"
-              onClick={() => {
-                history.push(`/courseview/${props.location.state.courseid}`);
-              }}
-            ></Button>
-          </Col>
-          <Col className="text-right">
-            <Button text="Submit"></Button>
-          </Col>
-        </Row>
-      </Form>
+            <Row className="mt-5">
+              <Col>
+                <Button
+                  text="Back"
+                  onClick={() => {
+                    history.push(
+                      `/courseview/${props.location.state.courseid}`,
+                    );
+                  }}
+                ></Button>
+              </Col>
+              <Col className="text-right">
+                <Button text="Submit"></Button>
+              </Col>
+            </Row>
+          </Form>
+        )}
+      </Formik>
       <br />
     </Container>
   );
